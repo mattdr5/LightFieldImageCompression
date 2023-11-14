e584c43c0e6407b33593a1ecac13eba74ee47b71
mattdr5 <dellarocca16@gmail.com>
2023-11-14 14:33:43 +0100

    Aggiunta gestione os

--- VideoCompressionUTVideo.py
+++ VideoCompressionUTVideo.py
@@ -4,21 +4,27 @@ import imageio.v3 as iio
 from pathlib import Path
 import imageio.plugins.pyav as pyav
 import av
-from PIL import Image,ImageChops
+from PIL import Image, ImageChops
 import numpy as np
 import os
 import subprocess
+import platform  # Import the platform module to check the operating system
 
-
-def comp_UTVIDEO(input_path,output_path):
+def comp_UTVIDEO(input_path, output_path):
     # Set the input and output file names
     input_file = input_path
     output_file = output_path
 
-    # Call ffmpeg to compress the video
-    subprocess.run(["./ffmpeg/bin/ffmpeg","-framerate", "120","-i", input_file,"-c:v", "utvideo",output_file])
+    # Check the operating system and set the path to the FFmpeg executable accordingly
+    if platform.system() == 'Windows':
+        ffmpeg_executable = "./ffmpeg/bin/ffmpeg.exe"
+    else:
+        ffmpeg_executable = "ffmpeg"
 
-    #calcolo compress ratio
+    # Call ffmpeg to compress the video
+    subprocess.run([ffmpeg_executable, "-framerate", "120", "-i", input_file, "-c:v", "utvideo", output_file])
+
+    # Calcolo compress ratio
     size = 0
     Folderpath = os.path.abspath(os.path.dirname(input_path))
     for path, dirs, files in os.walk(Folderpath):
@@ -26,5 +32,8 @@ def comp_UTVIDEO(input_path,output_path):
             fp = os.path.join(path, f)
             size += os.path.getsize(fp)
 
-    videoSize=os.stat(os.path.abspath(output_path)).st_size
-    print("Compression Ratio:" +str(size/videoSize))
+    videoSize = os.stat(os.path.abspath(output_path)).st_size
+    print("Compression Ratio:" + str(size / videoSize))
+
+# Example usage
+comp_UTVIDEO("input_video.mp4", "output_video.avi")
