96bd643416f6f0511ac39836f34f6e7c97883f78
Luke31999 <l.boffa1@studenti.unisa.it>
2023-12-01 17:05:40 +0100

    Removed codec SNOW

--- test_compression.py
+++ test_compression.py
@@ -1,8 +1,6 @@
 import os
-import subprocess
-import sys
 import pathlib
-import time
+import csv
 
 # Importa tutte le funzioni specifiche di compressione video
 from VideoCompressionHEVC import comp_HEVC
@@ -15,22 +13,16 @@ from VideoCompressionUTVideo import comp_UTVIDEO
 from VideoCompressionVP9 import comp_VP9
 from VideoCompressionVP9vls import comp_VP9_visuallyLS
 from VideoCompressionMagicYUV import comp_MagicYUV
-from VideoCompressionDirac import comp_Dirac, comp_Dirac_Lossless
 from VideoCompressionFLV1 import comp_FLV1
-from VideoCompressionSNOW import comp_Snow, comp_Snow_Lossless
 from VideoCompressionJPEG2000 import comp_jpeg2000, comp_JPEG2000_Lossless
 from VideoCompressionMPEG4 import comp_MPEG4
 from VideoCompressionCirrusLogic import comp_cljr
-from VideoCompressionProres import comp_ProRes
-from VideoCompressionMJPEG import comp_MJPEG
-from VideoCompressionFFVHUFF import comp_FFVHUFF
-from VideoCompressionLCL import comp_LCL
+from utils import datasets, get_valid_extension
 
-from random_dataset import randomizza_e_copia_files
 
-import csv
-import os
-
+#Definire la cartella di output per la compressione
+output_dir = "./compressione_test"
+os.makedirs(output_dir, exist_ok=True)
 
 def salva_risultati_compressione_csv(risultati, file_csv):
     """
@@ -62,65 +54,8 @@ def salva_risultati_compressione_csv(risultati, file_csv):
     except Exception as e:
         raise Exception(f"Errore durante la scrittura del file CSV: {str(e)}")
 
-
-def get_valid_extension(algo):
-    valid_extensions = {
-        "HEVC": ".mp4",
-        "HEVC-VS": ".mp4",
-        "AV1": ".mkv",
-        "AV1-VS": ".mkv",
-        "FFV1": ".avi",
-        "HUFFYUV": ".avi",
-        "UTVIDEO": ".avi",
-        "VP9": ".webm",
-        "VP9-VS": ".webm",
-        "MagicYUV": ".avi",
-        "Dirac": ".drc",
-        "Dirac-LS": ".drc",
-        "FLV1": ".flv",
-        "SNOW": ".avi",
-        "SNOW-LS": ".avi",
-        "JPEG2000": ".mp4",
-        "JPEG2000-LS": ".mp4",
-        "MPEG4": ".avi",
-        "CLJR": ".avi",
-        "ProRes": ".mov",
-        "MJPEG": ".avi",
-        "FFVHUFF": ".avi",
-        "LCL": ".avi"
-
-    }
-
-    return valid_extensions.get(algo)
-
-#Inserire il dataset da utilizzare, in particolare specificare i nomi delle immagini
-datasets = {
-    "ArtGallery2": "./dataset/ArtGallery2/Frame_%3d.png",
-    "ArtGallery2_random": "./dataset/ArtGallery2_random/Frame_%3d.png",
-    "Dragons": "./dataset/Dragons/dragons-%2d.png",
-    "Dragons_random": "./dataset/Dragons_random/Frame_%3d.png",
-    "Fish": "./dataset/Fish/fishi-%2d.png",
-    "Fish_random": "./dataset/Fish_random/Frame_%3d.png",
-    "Dice": "./dataset/Dice/dice-%2d.png",
-    "Dice_random": "./dataset/Dice_random/Frame_%3d.png",
-    "Messerschmitt": "./dataset/Messerschmitt/messerschmitt-%2d.png",
-    "Messerschmitt_random": "./dataset/Messerschmitt_random/Frame_%3d.png",
-    "Shrubbery": "./dataset/Shrubbery/shrubbery-%2d.png",
-    "Shrubbery_random": "./dataset/Shrubbery_random/Frame_%3d.png",
-    "bicycle": "./dataset/bicycle/input_Cam%3d.png",
-    "herbs": "./dataset/herbs/input_Cam%3d.png",
-    "bicycle_random": "./dataset/bicycle_random/Frame_%3d.png",
-    "herbs_random": "./dataset/herbs_random/Frame_%3d.png"
-}
-
 #Inserire qui gli algoritmi di compressione video da analizzare
-
-algorithms = ["FFV1", "HEVC", "HUFFYUV", "UTVIDEO", "VP9", "HEVC-VS", "VP9-VS", "FLV1", "CLJR", "MPEG4", "MJPEG", "ProRes", "MagicYUV", "FFVHUFF", "LCL"] 
-
-
-#Definire la cartella di output per la compressione
-output_dir = "./compressione_test"
-os.makedirs(output_dir, exist_ok=True)
+algorithms = ["HEVC", "HEVC-VS", "AV1", "AV1-VS", "FFV1", "HUFFYUV", "UTVIDEO", "VP9", "VP9-VS"]
 
 def run_video_compression(algo, input_path, output_extension, output_path):
     if output_extension is None:
@@ -178,31 +113,11 @@ def run_video_compression(algo, input_path, output_extension, output_path):
            return comp_MagicYUV(input_path, output_path)
         else:
             print("Estensione di output per MagicYUV deve essere .avi")
-    elif algo == "Dirac":
-        if pathlib.Path(output_path).suffix == ".drc":
-            return comp_Dirac(input_path, output_path)
-        else:
-            print("Estensione di output per Dirac deve essere .drc")
-    elif algo == "Dirac-LS":
-        if pathlib.Path(output_path).suffix == ".drc":
-            return comp_Dirac_Lossless(input_path, output_path)
-        else:
-            print("Estensione di output per Dirac-LS deve essere .drc")
     elif algo == "FLV1":
         if pathlib.Path(output_path).suffix == ".flv":
            return comp_FLV1(input_path, output_path)
         else:
             print("Estensione di output per FLV1 deve essere .flv")
-    elif algo == "SNOW":
-        if pathlib.Path(output_path).suffix == ".avi":
-            return comp_Snow(input_path, output_path)
-        else:
-            print("Estensione di output per SNOW deve essere .avi")
-    elif algo == "SNOW-LS":
-        if pathlib.Path(output_path).suffix == ".avi":
-            return comp_Snow_Lossless(input_path, output_path)
-        else:
-            print("Estensione di output per SNOW-LS deve essere .avi")
     elif algo == "JPEG2000":
         if pathlib.Path(output_path).suffix == ".mp4":
             return comp_jpeg2000(input_path, output_path)
@@ -223,26 +138,6 @@ def run_video_compression(algo, input_path, output_extension, output_path):
             return comp_cljr(input_path, output_path)
         else:
             print("Estensione di output per CLJR deve essere .avi")
-    elif algo == "ProRes":
-        if pathlib.Path(output_path).suffix == ".mov":
-            return comp_ProRes(input_path, output_path)
-        else:
-            print("Estensione di output per ProRes deve essere .mov")
-    elif algo == "MJPEG":
-        if pathlib.Path(output_path).suffix == ".avi":
-            return comp_MJPEG(input_path, output_path)
-        else:
-            print("Estensione di output per MJPEG deve essere .avi")
-    elif algo == "FFVHUFF":
-        if pathlib.Path(output_path).suffix == ".avi":
-            return comp_FFVHUFF(input_path, output_path)
-        else:
-            print("Per FFVHUFF l'estensione del file in output deve essere .avi")
-    elif algo == "LCL":
-        if pathlib.Path(output_path).suffix == ".avi":
-            return comp_LCL(input_path, output_path)
-        else:
-            print("Per LCL l'estensione del file in output deve essere .avi")
     else:
         print(f"Algoritmo non riconosciuto: {algo}")
 
