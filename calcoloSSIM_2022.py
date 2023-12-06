7c08b1e4f72ee799ee6185e61117c07c9d40437a
VinzOmega <vincenzodileo98@gmail.com>
2023-12-06 11:50:18 +0100

    Calcolo SSIM 2022

---
+++ calcoloSSIM_2022.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+
+def run_ffmpeg_command(dataset, codec, output_folder):
+    input_folder = f"decompressione_test/{dataset}/{codec}/Frame_%d.png"
+    
+    if dataset == 'ArtGallery2':
+        dataset_folder = f"dataset/{dataset}/Frame_%3d.png"
+    elif dataset == 'Dragons':
+        dataset_folder = f"dataset/{dataset}/dragons-%2d.png"
+    elif dataset == 'OpEx':
+        dataset_folder = f"dataset/{dataset}/%d.png"
+
+    stats_filename = f"{output_folder}/ssim_{dataset}_{codec}.txt"
+    command = f"ffmpeg -i {input_folder} -i {dataset_folder} -lavfi ssim=stats_file={stats_filename} -f null -"
+    subprocess.run(command, shell=True)
+
+def main():
+    datasets = ['OpEx', 'ArtGallery2', 'Dragons']
+    codecs = ['HEVC', 'AV1', 'VP9', 'HEVC-VS', 'AV1-VS', 'VP9-VS']
+    output_folder = 'ssim_2022'
+
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+
+    for dataset in datasets:
+        for codec in codecs:
+            run_ffmpeg_command(dataset, codec, output_folder)
+
+if __name__ == "__main__":
+    main()
