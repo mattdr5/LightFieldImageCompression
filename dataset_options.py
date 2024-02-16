73bfeae4ef08db39acd67d81ef9f8a964b21126c
VinzOmega <vincenzodileo98@gmail.com>
2024-02-16 21:30:30 +0100

    Aggiunta nuovi risultati

--- dataset_options.py
+++ dataset_options.py
@@ -38,14 +38,14 @@ DATASET_OPTIONS = {
         'ProRes': ["-q:v", "64"],
         'MPEG4': ["-q:v", "14"],
     },
-    'cobblestone': {       #0,90, PSNR uguali      Prores può scendere anche a 0.85 
+    'cobblestone': {       #0,90      
         'FLV1': ["-q:v", "10"],
         'MJPEG': ["-q:v", "13"],
         'ProRes': ["-q:v", "36"],
         'MPEG4': ["-q:v", "10"],
     },
-    'cobblestone_random': {        #0,90, PSNR uguali      Prores può scendere anche a 0.85 
-        'FLV1': ["-q:v", "10"],
+    'cobblestone_random': {        #0,90      
+       'FLV1': ["-q:v", "10"],
         'MJPEG': ["-q:v", "13"],
         'ProRes': ["-q:v", "36"],
         'MPEG4': ["-q:v", "10"],
@@ -62,31 +62,31 @@ DATASET_OPTIONS = {
         'ProRes': ["-q:v", "64"],
         'MPEG4': ["-q:v", "12"],
     },
-    'dragons': {          #0.90,  PSNR uguali
+    'dragons': {          #0.90
         'FLV1': ["-q:v", "13"],
         'MJPEG': ["-q:v", "19"],
         'ProRes': ["-q:v", "56"],
         'MPEG4': ["-q:v", "13"],
     },
-    'dragons_random': {         #0.90,  PSNR uguali
+    'dragons_random': {         #0.90
         'FLV1': ["-q:v", "13"],
         'MJPEG': ["-q:v", "19"],
         'ProRes': ["-q:v", "56"],
         'MPEG4': ["-q:v", "13"],
     },
-    'fish': {          #0.95,  PSNR uguali  
+    'fish': {          #0.95 
         'FLV1': ["-q:v", "16"],
         'MJPEG': ["-q:v", "23"],
         'ProRes': ["-q:v", "64"],
         'MPEG4': ["-q:v", "16"],
     },
-    'fish_random': {         #0.95,  PSNR uguali
+    'fish_random': {         #0.95
         'FLV1': ["-q:v", "16"],
         'MJPEG': ["-q:v", "23"],
         'ProRes': ["-q:v", "64"],
         'MPEG4': ["-q:v", "16"],
     },
-    'mannequin': {          #0.90,  PSNR uguali
+    'mannequin': {          #0.90
         'FLV1': ["-q:v", "13"],
         'MJPEG': ["-q:v", "18"],
         'ProRes': ["-q:v", "56"],
@@ -98,13 +98,13 @@ DATASET_OPTIONS = {
         'ProRes': ["-q:v", "56"],
         'MPEG4': ["-q:v", "11"],
     },
-    'messerschmitt': {     #0.90            Prores può scendere a 0.86
+    'messerschmitt': {         #0.90            
         'FLV1': ["-q:v", "7"],
         'MJPEG': ["-q:v", "11"],
         'ProRes': ["-q:v", "34"],
         'MPEG4': ["-q:v", "7"],
     },
-    'messerschmitt_random': {        #0.90            Prores può scendere a 0.86
+    'messerschmitt_random': {        #0.90           
         'FLV1': ["-q:v", "7"],
         'MJPEG': ["-q:v", "11"],
         'ProRes': ["-q:v", "34"],
@@ -122,18 +122,57 @@ DATASET_OPTIONS = {
         'ProRes': ["-q:v", "64"],
         'MPEG4': ["-q:v", "8"],
     },
-    #'cobblestone': {       # VERSIONE SSIM 0,85, PSNR uguali     
+    #'cobblestone': {          # VERSIONE SSIM 0,85    
     #    'FLV1': ["-q:v", "16"],
     #    'MJPEG': ["-q:v", "21"],
     #    'ProRes': ["-q:v", "64"],
     #    'MPEG4': ["-q:v", "16"],
     #},
-    #'cobblestone': {       # VERSIONE SSIM 0,95, PSNR uguali     
+    #'cobblestone_random': {      # VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "15"],
+    #    'MJPEG': ["-q:v", "21"],
+    #    'ProRes': ["-q:v", "64"],
+    #    'MPEG4': ["-q:v", "14"],
+    #},
+    #'cobblestone': {          # VERSIONE SSIM 0,95   
     #    'FLV1': ["-q:v", "6"],
     #    'MJPEG': ["-q:v", "7"],
     #    'ProRes': ["-q:v", "17"],
     #    'MPEG4': ["-q:v", "6"],
-    #}
+    #},
+    #'cobblestone_random': {         # VERSIONE SSIM 0,95 
+    #    'FLV1': ["-q:v", "5"],
+    #    'MJPEG': ["-q:v", "7"],
+    #    'ProRes': ["-q:v", "17"],
+    #    'MPEG4': ["-q:v", "5"],
+    #},
+    #'messerschmitt': {          # VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "14"],
+    #    'MJPEG': ["-q:v", "23"],
+    #    'ProRes': ["-q:v", "64"],
+    #    'MPEG4': ["-q:v", "13"],
+    #},
+    #'messerschmitt_random': {          # VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "13"],
+    #    'MJPEG': ["-q:v", "23"],
+    #    'ProRes': ["-q:v", "64"],
+    #    'MPEG4': ["-q:v", "12"],
+    #},
+    #'messerschmitt': {          # VERSIONE SSIM 0,95 
+    #    'FLV1': ["-q:v", "3"],
+    #    'MJPEG': ["-q:v", "5"],
+    #    'ProRes': ["-q:v", "14"],
+    #    'MPEG4': ["-q:v", "3"],
+    #},
+    #'messerschmitt_random': {          # VERSIONE SSIM 0,95 
+    #    'FLV1': ["-q:v", "3"],
+    #    'MJPEG': ["-q:v", "5"],
+    #    'ProRes': ["-q:v", "14"],
+    #    'MPEG4': ["-q:v", "3"],
+    #},
+
+
+
 
     # Altri dataset con le relative opzioni
 }
