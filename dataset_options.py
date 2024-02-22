fd4a893545a103550853d7b3037b3c66b14719d2
VinzOmega <vincenzodileo98@gmail.com>
2024-02-22 16:53:06 +0100

    Aggiornamento risultati

--- dataset_options.py
+++ dataset_options.py
@@ -3,10 +3,10 @@
 
 DATASET_OPTIONS = {
     'artgallery2': {      #0.93
-        'FLV1': ["-q:v", "13"],
-        'MJPEG': ["-q:v", "16"],
-        'ProRes': ["-q:v", "62"],
-        'MPEG4': ["-q:v", "12"],
+       'FLV1': ["-q:v", "13"],
+       'MJPEG': ["-q:v", "16"],
+       'ProRes': ["-q:v", "62"],
+       'MPEG4': ["-q:v", "12"],
     },
     'artgallery2_random': {      #0.93
         'FLV1': ["-q:v", "13"],
@@ -14,13 +14,13 @@ DATASET_OPTIONS = {
         'ProRes': ["-q:v", "62"],
         'MPEG4': ["-q:v", "12"],
     },
-    'blob': {        #0,93, PSNR uguali      
+    'blob': {        #0,93     
         'FLV1': ["-q:v", "14"],
         'MJPEG': ["-q:v", "21"],
         'ProRes': ["-q:v", "64"],
         'MPEG4': ["-q:v", "12"],
     },
-    'blob_random': {         #0,93, PSNR uguali  
+    'blob_random': {         #0,93
         'FLV1': ["-q:v", "11"],
         'MJPEG': ["-q:v", "21"],
         'ProRes': ["-q:v", "64"],
@@ -170,8 +170,86 @@ DATASET_OPTIONS = {
     #    'ProRes': ["-q:v", "14"],
     #    'MPEG4': ["-q:v", "3"],
     #},
-
-
+    #'dragons': {       #VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "25"],
+    #    'MJPEG': ["-q:v", "33"],
+    #    'MPEG4': ["-q:v", "24"],
+    #},
+    #'dragons': {       #VERSIONE SSIM 0,95
+    #    'FLV1': ["-q:v", "5"],
+    #    'MJPEG': ["-q:v", "7"],
+    #    'MPEG4': ["-q:v", "5"],
+    #},
+    #'dragons_random': {       #VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "25"],
+    #    'MJPEG': ["-q:v", "33"],
+    #    'MPEG4': ["-q:v", "24"],
+    #},
+    #'dragons_random': {       #VERSIONE SSIM 0,95
+    #    'FLV1': ["-q:v", "5"],
+    #    'MJPEG': ["-q:v", "7"],
+    #    'MPEG4': ["-q:v", "5"],
+    #},
+    #'mannequin': {          #VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "22"],
+    #    'MJPEG': ["-q:v", "33"],
+    #    'MPEG4': ["-q:v", "21"],
+    #},
+    #'mannequin': {          #VERSIONE SSIM 0,95
+    #    'FLV1': ["-q:v", "6"],
+    #    'MJPEG': ["-q:v", "8"],
+    #    'MPEG4': ["-q:v", "6"],
+    #},
+    #'mannequin_random': {          #VERSIONE SSIM 0,85
+    #    'FLV1': ["-q:v", "20"],
+    #    'MJPEG': ["-q:v", "33"],
+    #    'MPEG4': ["-q:v", "19"],
+    #},
+    #'mannequin_random': {          #VERSIONE SSIM 0,95
+    #    'FLV1': ["-q:v", "5"],
+    #    'MJPEG': ["-q:v", "8"],
+    #    'MPEG4': ["-q:v", "5"],
+    #},
+    #'artgallery2': {      #VERSIONE SSIM 0,90
+    #   'FLV1': ["-q:v", "30"],
+    #   'MJPEG': ["-q:v", "32"],
+    #   'MPEG4': ["-q:v", "26"],
+    #},
+    #'artgallery2': {      #VERSIONE SSIM 0,95
+    #   'FLV1': ["-q:v", "5"],
+    #   'MJPEG': ["-q:v", "7"],
+    #   'MPEG4': ["-q:v", "5"],
+    #},
+    #'artgallery2_random': {      #VERSIONE SSIM 0,90
+    #    'FLV1': ["-q:v", "30"],
+    #    'MJPEG': ["-q:v", "32"],
+    #    'MPEG4': ["-q:v", "26"],
+    #},
+    #'artgallery2_random': {      #VERSIONE SSIM 0,95
+    #   'FLV1': ["-q:v", "5"],
+    #   'MJPEG': ["-q:v", "7"],
+    #   'MPEG4': ["-q:v", "5"],
+    #},
+    #'blob': {      #VERSIONE SSIM 0,90
+    #   'FLV1': ["-q:v", "23"],
+    #   'MJPEG': ["-q:v", "32"],
+    #   'MPEG4': ["-q:v", "22"],
+    #},
+    #'blob': {      #VERSIONE SSIM 0,95
+    #   'FLV1': ["-q:v", "9"],
+    #   'MJPEG': ["-q:v", "13"],
+    #   'MPEG4': ["-q:v", "8"],
+    #},
+    #'blob_random': {      #VERSIONE SSIM 0,90
+    #   'FLV1': ["-q:v", "19"],
+    #   'MJPEG': ["-q:v", "32"],
+    #   'MPEG4': ["-q:v", "15"],
+    #},
+    #'blob_random': {      #VERSIONE SSIM 0,95
+    #   'FLV1': ["-q:v", "7"],
+    #   'MJPEG': ["-q:v", "12"],
+    #   'MPEG4': ["-q:v", "6"],
+    #}
 
 
     # Altri dataset con le relative opzioni
